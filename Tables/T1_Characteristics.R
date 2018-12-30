9c1a2d9feaeacc5680f83379dbdc93f796553068
robertjirsaraie21@gmail.com <rjirsaraie@chead.uphs.upenn.edu>
2018-12-30 17:28:56 -0500

    Computes Sample Demographics and Clinical Characteristics

---
+++ Tables/T1_Characteristics.R
@@ -0,0 +1,201 @@
+###################################################################################################
+##########################             GRMPY - Create Tables             ##########################
+##########################              Robert Jirsaraie                 ##########################
+##########################             rjirsaraie@upenn.edu              ##########################
+##########################                  09/24/2018                   ##########################
+###################################################################################################
+#####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  #####
+###################################################################################################
+
+###############################################
+### Read in Data and Load Relevent Packages ###
+###############################################
+
+rds<-readRDS("/data/jux/BBL/projects/jirsaraieStructuralIrrit/data/processedData/longitudinal/n137_Demo+Psych+DX+QA_20180531.rds")
+
+library(effsize)
+library(dplyr)
+library(lsr)
+
+#####################################################
+### Create Datasets Seperated by Diagnosis Groups ###
+#####################################################
+
+NC <- rds[which(rds$TP1_goassessSmryNCvsDX ==0 & rds$TP2_dx_NCvsDX ==0),]
+dim(NC)
+DX <- rds[which(rds$TP1_goassessSmryNCvsDX ==1 & rds$TP2_dx_NCvsDX ==1),]
+dim(DX)
+Resilient <- rds[which(rds$TP1_goassessSmryNCvsDX ==1 & rds$TP2_dx_NCvsDX ==0),]
+dim(Resilient)
+Emerge <- rds[which(rds$TP1_goassessSmryNCvsDX ==0 & rds$TP2_dx_NCvsDX == 1),]
+dim(Emerge)
+
+##############################################
+### Create New Variable of Diagnosis Group ###
+##############################################
+
+rds$DiagnosisGroup<-0
+rds[which(rds$TP1_goassessSmryNCvsDX ==0 & rds$TP2_dx_NCvsDX ==0),88]<-1 #NC
+rds[which(rds$TP1_goassessSmryNCvsDX ==1 & rds$TP2_dx_NCvsDX ==1),88]<-2 #DX
+rds[which(rds$TP1_goassessSmryNCvsDX ==1 & rds$TP2_dx_NCvsDX ==0),88]<-3 #Resilient
+rds[which(rds$TP1_goassessSmryNCvsDX ==0 & rds$TP2_dx_NCvsDX ==1),88]<-4 #Emergent
+rds$DiagnosisGroup<-as.factor(rds$DiagnosisGroup)
+
+###########################################
+### Aanlyze Differences in Baseline Age ###
+###########################################
+
+summary(NC$TP1_ageAtScan1)
+sd(NC$TP1_ageAtScan1)
+summary(DX$TP1_ageAtScan1)
+sd(DX$TP1_ageAtScan1)
+summary(Resilient$TP1_ageAtScan1)
+sd(Resilient$TP1_ageAtScan1)
+summary(Emerge$TP1_ageAtScan1)
+sd(Emerge$TP1_ageAtScan1)
+
+summary(aov(rds$TP1_ageAtScan1~rds$DiagnosisGroup)) #0.15
+etaSquared(aov(rds$TP1_ageAtScan1~rds$DiagnosisGroup)) #0.04
+
+############################################
+### Aanlyze Differences in Follow Up Age ###
+############################################
+
+summary(NC$TP2_ScanAgeYears)
+sd(NC$TP2_ScanAgeYears)
+summary(DX$TP2_ScanAgeYears)
+sd(DX$TP2_ScanAgeYears)
+summary(Resilient$TP2_ScanAgeYears)
+sd(Resilient$TP2_ScanAgeYears)
+summary(Emerge$TP2_ScanAgeYears)
+sd(Emerge$TP2_ScanAgeYears)
+
+summary(aov(rds$TP2_ScanAgeYears~rds$DiagnosisGroup)) #0.04
+etaSquared(aov(rds$TP2_ScanAgeYears~rds$DiagnosisGroup)) #0.06
+
+############################################
+### Aanlyze Differences in Sex per Group ###
+############################################
+
+summary(NC$TP2_sex)
+summary(DX$TP2_sex)
+summary(Resilient$TP2_sex)
+summary(Emerge$TP2_sex)
+
+chisq.test(table(rds$DiagnosisGroup,rds$TP2_sex)) #0.29
+
+###############################################
+### Aanlyze Differences in Father Education ###
+###############################################
+
+summary(NC$TP2_dad_educ)
+sd(NC$TP2_dad_educ, na.rm=TRUE)
+summary(DX$TP2_dad_educ)
+sd(DX$TP2_dad_educ, na.rm=TRUE)
+summary(Resilient$TP2_dad_educ)
+sd(Resilient$TP2_dad_educ, na.rm=TRUE)
+summary(Emerge$TP2_dad_educ)
+sd(Emerge$TP2_dad_educ, na.rm=TRUE)
+
+summary(aov(rds$TP2_dad_educ~rds$DiagnosisGroup)) #0.25
+etaSquared(aov(rds$TP2_dad_educ~rds$DiagnosisGroup)) #0.03
+
+###############################################
+### Aanlyze Differences in Mother Education ###
+###############################################
+
+summary(NC$TP2_mom_educ)
+sd(NC$TP2_mom_educ, na.rm=TRUE)
+summary(DX$TP2_mom_educ)
+sd(DX$TP2_mom_educ, na.rm=TRUE)
+summary(Resilient$TP2_mom_educ)
+sd(Resilient$TP2_mom_educ, na.rm=TRUE)
+summary(Emerge$TP2_mom_educ)
+sd(Emerge$TP2_mom_educ, na.rm=TRUE)
+
+summary(aov(rds$TP2_mom_educ~rds$DiagnosisGroup)) #0.03
+etaSquared(aov(rds$TP2_mom_educ~rds$DiagnosisGroup)) #0.07
+
+####################################################
+### Aanlyze Differences in Baseline Irritability ###
+####################################################
+
+summary(NC$TP1_IrritabilitySum)
+sd(NC$TP1_IrritabilitySum, na.rm=TRUE)
+summary(DX$TP1_IrritabilitySum)
+sd(DX$TP1_IrritabilitySum, na.rm=TRUE)
+summary(Resilient$TP1_IrritabilitySum)
+sd(Resilient$TP1_IrritabilitySum, na.rm=TRUE)
+summary(Emerge$TP1_IrritabilitySum)
+sd(Emerge$TP1_IrritabilitySum, na.rm=TRUE)
+
+summary(aov(rds$TP1_IrritabilitySum~rds$DiagnosisGroup)) #<0.0001
+etaSquared(aov(rds$TP1_IrritabilitySum~rds$DiagnosisGroup)) #0.35
+
+####################################################
+### Aanlyze Differences in Followup Irritability ###
+####################################################
+
+summary(NC$TP2_ari_total)
+sd(NC$TP2_ari_total, na.rm=TRUE)
+summary(DX$TP2_ari_total)
+sd(DX$TP2_ari_total, na.rm=TRUE)
+summary(Resilient$TP2_ari_total)
+sd(Resilient$TP2_ari_total, na.rm=TRUE)
+summary(Emerge$TP2_ari_total)
+sd(Emerge$TP2_ari_total, na.rm=TRUE)
+
+summary(aov(rds$TP2_ari_total~rds$DiagnosisGroup)) #0.06
+etaSquared(aov(rds$TP2_ari_total~rds$DiagnosisGroup)) #0.05
+
+##################################################
+### Aanlyze Differences in Followup Depression ###
+##################################################
+
+summary(NC$TP2_bdi_total)
+sd(NC$TP2_bdi_total, na.rm=TRUE)
+summary(DX$TP2_bdi_total)
+sd(DX$TP2_bdi_total, na.rm=TRUE)
+summary(Resilient$TP2_bdi_total)
+sd(Resilient$TP2_bdi_total, na.rm=TRUE)
+summary(Emerge$TP2_bdi_total)
+sd(Emerge$TP2_bdi_total, na.rm=TRUE)
+
+summary(aov(rds$TP2_bdi_total~rds$DiagnosisGroup)) #<0.0001
+etaSquared(aov(rds$TP2_bdi_total~rds$DiagnosisGroup)) #0.08
+
+#####################################################
+### Aanlyze Differences in Followup ADHD Symptoms ###
+#####################################################
+
+summary(NC$TP2_adhd_total)
+sd(NC$TP2_adhd_total, na.rm=TRUE)
+summary(DX$TP2_adhd_total)
+sd(DX$TP2_adhd_total, na.rm=TRUE)
+summary(Resilient$TP2_adhd_total)
+sd(Resilient$TP2_adhd_total, na.rm=TRUE)
+summary(Emerge$TP2_adhd_total)
+sd(Emerge$TP2_adhd_total, na.rm=TRUE)
+
+summary(aov(rds$TP2_adhd_total~rds$DiagnosisGroup)) #0.02
+etaSquared(aov(rds$TP2_adhd_total~rds$DiagnosisGroup)) #0.07
+
+########################################################
+### Aanlyze Differences in Followup Anxiety Symptoms ###
+########################################################
+
+summary(NC$TP2_scared_total)
+sd(NC$TP2_scared_total, na.rm=TRUE)
+summary(DX$TP2_scared_total)
+sd(DX$TP2_scared_total, na.rm=TRUE)
+summary(Resilient$TP2_scared_total)
+sd(Resilient$TP2_scared_total, na.rm=TRUE)
+summary(Emerge$TP2_scared_total)
+sd(Emerge$TP2_scared_total, na.rm=TRUE)
+
+summary(aov(rds$TP2_scared_total~rds$DiagnosisGroup)) #0.02
+etaSquared(aov(rds$TP2_scared_total~rds$DiagnosisGroup)) #0.07
+
+###################################################################################################
+#####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  #####
+###################################################################################################
