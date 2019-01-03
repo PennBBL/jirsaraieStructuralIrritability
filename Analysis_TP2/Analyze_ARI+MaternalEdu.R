656774a1d6149c2b79825938631a7b6f678c4a25
robertjirsaraie21@gmail.com <rjirsaraie@chead.uphs.upenn.edu>
2019-01-03 00:00:47 -0500

    Executes Sensitivity Analysis Contoling for Maternal Education

--- Analysis_TP2/Analyze_ARI+MaternalEdu.R
+++ Analysis_TP2/Analyze_ARI+MaternalEdu.R
@@ -37,7 +37,7 @@ inputPath<-'/data/jux/BBL/projects/jirsaraieStructuralIrrit/data/processedData/f
 subjDataName<-'/data/jux/BBL/projects/jirsaraieStructuralIrrit/data/processedData/follow-up/n141_Demo+Psych+DX+QA_20180724.rds'
 OutDirRoot <- '/data/jux/BBL/projects/jirsaraieStructuralIrrit/output/FINAL/follow-up'
 
-covsFormula <- "~s(ScanAgeYears,k=4)+sex+rating+ari_log+TP2_mom_educ"
+covsFormula <- "~s(ScanAgeYears,k=4)+sex+rating+ari_log+mom_educ"
 
 ##############################################################################
 ################              Adjust Parameters               ################
